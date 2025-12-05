c7b6f7882d894e9888e09ee26f1cac04123c1c73
hoang44544 <hoang44544@gmail.com>
2025-12-05 10:41:09 +0700

    Implement input validation for array elements
    Added input validation for array elements to handle non-integer values.

--- bai64.c
+++ bai64.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-// Bài 64. Viết chương trình nhập mảng 1 chiều A có N phần tử  có sử dụng hàm(với N 
-// nhập từ bàn phím). :   
-// a. Xuất các phần tử đã nhập ra màn hình. 
-// b. Xuất ra màn hình số nguyên tố cuối cùng của mảng (nếu có),còn không xuất 
-// ra dòng “Không có số nguyên tố trong mảng”  ? 
+#include <string.h>
+#include <stdlib.h>
+
 int isPrime(int num) {
     if (num < 2) return 0;
     for (int i = 2; i * i <= num; i++) {
@@ -11,36 +9,33 @@ int isPrime(int num) {
     }
     return 1;
 }
+
 int main() {
     int N;
     printf("Nhap so luong phan tu N: ");
     scanf("%d", &N);
-
+    char input[100];
     int A[N];
     printf("Nhap %d phan tu cho mang:\n", N);
     for (int i = 0; i < N; i++) {
         printf("A[%d] = ", i);
-        scanf("%d", &A[i]);
+        scanf("%s", input);
+        if (strchr(input, '.') != NULL) {
+            A[i] = -1; //đánh dấu kh hợp lệ
+            continue;
+        }
+        A[i] = atoi(input);
     }
-
-    printf("Cac phan tu trong mang la:\n");
-    for (int i = 0; i < N; i++) {
-        printf("%d ", A[i]);
-    }
-    printf("\n");
-
     int lastPrime = -1;
     for (int i = 0; i < N; i++) {
-        if (isPrime(A[i])) {
+        if (A[i] != -1 && isPrime(A[i])) {
             lastPrime = A[i];
         }
     }
-
     if (lastPrime != -1) {
         printf("So nguyen to cuoi cung trong mang la: %d\n", lastPrime);
     } else {
         printf("Khong co so nguyen to trong mang.\n");
     }
-
     return 0;
 }
