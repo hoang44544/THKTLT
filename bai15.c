c609a36f578fafa129ef340583f2634dd6ca3bfb
hoang44544 <hoang44544@gmail.com>
2025-12-05 10:39:22 +0700

    Update bai15.c

--- bai15.c
+++ bai15.c
@@ -1,24 +1,35 @@
 #include <stdio.h>
 #include <math.h>
-//Bài 15. Cho tam giác ABC với 3 đỉnh A,B,C có tọa độ là:(x1,y1),(x2,y2),(x3,y3)
-//a. Diện tích của tam giác 
-//b. Chu vi của tam giác 
-//Viết chương trình sử dụng hàm để giải bài toán này. 
+
+double khoang_cach(double x1, double y1, double x2, double y2) {
+    return sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+}
+double dien_tich(double a, double b, double c) {
+    double p = (a + b + c) / 2;
+    return sqrt(p * (p - a) * (p - b) * (p - c));
+}
+int la_tam_giac(double a, double b, double c) {
+    return (a + b > c && a + c > b && b + c > a);
+}
+
 int main() {
-    int x1, y1, x2, y2, x3, y3;
+    double x1, y1, x2, y2, x3, y3;
     printf("Nhap toa do diem A (x1 y1): ");
-    scanf("%d %d", &x1, &y1);   
+    scanf("%lf %lf", &x1, &y1);
     printf("Nhap toa do diem B (x2 y2): ");
-    scanf("%d %d", &x2, &y2);
+    scanf("%lf %lf", &x2, &y2);
     printf("Nhap toa do diem C (x3 y3): ");
-    scanf("%d %d", &x3, &y3);
-    float a = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
-    float b = sqrt((x3 - x2) * (x3 - x2) + (y3 - y2) * (y3 - y2));
-    float c = sqrt((x1 - x3) * (x1 - x3) + (y1 - y3) * (y1 - y3));
-    float p = (a + b + c) / 2;
-    float dien_tich = sqrt(p * (p - a) * (p - b) * (p - c));
-    float chu_vi = a + b + c;
-    printf("Dien tich: %.2f\n", dien_tich);
-    printf("Chu vi: %.2f\n", chu_vi);
+    scanf("%lf %lf", &x3, &y3);
+    double a = khoang_cach(x1, y1, x2, y2);
+    double b = khoang_cach(x2, y2, x3, y3);
+    double c = khoang_cach(x3, y3, x1, y1);
+    if (!la_tam_giac(a, b, c)) {
+        printf("Ba diem khong tao thanh tam giac.\n");
+        return 0;
+    }
+    double S = dien_tich(a, b, c);
+    double P = a + b + c;
+    printf("Dien tich tam giac: %.2lf\n", S);
+    printf("Chu vi tam giac: %.2lf\n", P);
     return 0;
 }
